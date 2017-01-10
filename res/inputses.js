46dcb51f0eea88065d16db495eca21c568cbceac
Bruno Henriques <bruno.s.henriques@tecnico.ulisboa.pt>
2017-01-10 22:29:35 +0000

    Renaming Cleanup code Update normalize.css to 4.2.0

--- res/inputses.js
+++ res/inputses.js
@@ -9,8 +9,8 @@ elTime.addEventListener("input", onChangedTime);
 elDist.addEventListener("input", onChangedDist);
 elSpeed.addEventListener("input", onChangedSpeed);
 
-elDist.addEventListener("wheel", scrollDist);
-elSpeed.addEventListener("wheel", scrollSpeed);
+elDist.addEventListener("wheel", onScrollDist);
+elSpeed.addEventListener("wheel", onScrollSpeed);
 
 function parseTime(time){
     let splits = time.split(":");
@@ -22,28 +22,25 @@ function parseTime(time){
 /* ============================== */
 
 function onChangedTime(e){
-    // console.log(e.target.value);
     let speed = calcSpeed(parseTime(elTime.value), elDist.value);
     setSpeed(speed, false);
 }
 
 function onChangedDist(e){
-    // console.log(e.target.value);
     setDist(e.target.value, true);
 }
 
 function onChangedSpeed(e){
-    // console.log(e.target.value);
     setSpeed(e.target.value, true);
 }
 
-function scrollDist(e){
+function onScrollDist(e){
     let scroll = parseInt(e.target.step, 10) * -Math.sign(e.deltaY);
     e.target.value = parseInt(e.target.value, 10) + scroll;
     setDist(e.target.value, true);
 }
 
-function scrollSpeed(e){
+function onScrollSpeed(e){
     let scroll = parseInt(e.target.step, 10) * -Math.sign(e.deltaY);
     e.target.value = parseInt(e.target.value, 10) + scroll;
     setSpeed(e.target.value, true);
--- res/scripts.js
+++ res/scripts.js
@@ -4,22 +4,22 @@ const sec = 1;
 const min = 60*sec;
 const hour = 60*min;
 
-var f00 = (n) => (n<10) ? "0"+n : String(n);
-var calcTime = (dist, speed) => (dist / speed) * hour;  // seconds
-var calcDist = (time, speed) => speed * time / hour;    // meters
-var calcPace = (time, dist) => time / dist * 1000;      // seconds(/km)
-var calcSpeed = (time, dist) => dist * hour / time;     // meters(/hour)
+const f00 = (n) => (n<10) ? "0"+n : String(n);
+const calcTime = (dist, speed) => (dist / speed) * hour;  // seconds
+const calcDist = (time, speed) => speed * time / hour;    // meters
+const calcPace = (time, dist) => time / dist * 1000;      // seconds(/km)
+const calcSpeed = (time, dist) => dist * hour / time;     // meters(/hour)
 
 /* ============================== */
 
-function setTime(time, changed){
+function setTime(time){
     let hms = [time/hour, Math.floor(time/min)%60, time%60].map(Math.floor);
     elTime.value = hms.map(f00).join(":");
 }
 
 function setDist(dist, changed){
     let spanDist = document.getElementById("spanDist");
-    spanDist.innerHTML = (dist/1000).toFixed(1) + "km";
+    spanDist.innerHTML = "Distance " + (dist/1000).toFixed(1) + "km";
     elDist.value = dist;
     
     if (timeFix.checked){
@@ -27,20 +27,18 @@ function setDist(dist, changed){
         if (changed) setSpeed(speed, false);
     } else{
         let time = calcTime(elDist.value, elSpeed.value);
-        setTime(time, false);
+        setTime(time);
     }
 }
 
 function setSpeed(speed, changed){
     let spanPace = document.getElementById("spanPace");
-    let spanSpeed = document.getElementById("spanSpeed");
 
     let kph = (speed/1000);
     let mpk = (60/kph);
     let ms = [mpk, (mpk%1)*60].map(Math.floor).map(f00).join(":");
 
-    spanPace.innerHTML = ms + "/km";
-    spanSpeed.innerHTML = kph.toFixed(1) + "kph";
+    spanPace.innerHTML = "Pace " + ms + "/km (" + kph.toFixed(1) + "kph)";
     elSpeed.value = speed;
 
     if (timeFix.checked){
@@ -48,6 +46,6 @@ function setSpeed(speed, changed){
         if (changed) setDist(dist, false);
     } else{
         let time = calcTime(elDist.value, elSpeed.value);
-        setTime(time, true);
+        setTime(time);
     }
 }
