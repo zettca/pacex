98a5f3fd06b2b3ca8d832e3ad560a9e31d49dc1e
Bruno <zettca@gmail.com>
2020-09-03 19:27:50 +0100

    feat: add auto dark theme

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useMemo } from "react";
 import ReactDOM from "react-dom";
-import { CssBaseline } from "@material-ui/core";
+import { CssBaseline, useMediaQuery } from "@material-ui/core";
 import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import renderLayout from "./layout/renderLayout";
 import Main from "./components/Main";
 import * as serviceWorker from "./serviceWorker";
 
 const App = () => {
-  const theme = createMuiTheme();
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = useMemo(
+    () => createMuiTheme({ palette: { type: prefersDarkMode ? "dark" : "light" } }),
+    [prefersDarkMode],
+  );
 
   return (
     <ThemeProvider theme={theme}>
