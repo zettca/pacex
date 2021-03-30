8072924561edc505143500bd197f29e164dc231b
Bruno <zettca@gmail.com>
2021-03-30 20:02:34 +0100

    linting depth pls? 🤷

--- src/components/SliderPicker/SliderPicker.js
+++ src/components/SliderPicker/SliderPicker.js
@@ -37,7 +37,7 @@ const SliderPicker = (props) => {
 
   useEffect(() => {
     setValue(value);
-  }, [value]);
+  }, [value, setValue]);
 
   const handleChange = (evt, val) => {
     sliderProps.onChange(evt, val);
