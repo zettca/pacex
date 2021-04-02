50eed69a8a418fc1af1638879e3166258fd040dd
Bruno <zettca@gmail.com>
2021-04-02 14:21:53 +0100

    fix(tests): lang agnistic tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,35 +1,34 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import Main from "./components/Main";
+
+jest.mock("react-i18next", () => ({
+  useTranslation: () => ({
+    t: (str) => str,
+    i18n: {
+      changeLanguage: () => new Promise(() => {}),
+    },
+  }),
+}));
 
 describe("App renders", () => {
   test("main", () => {
-    render(<App />);
+    render(<Main />);
     const main = screen.getByRole("main");
     expect(main).toBeInTheDocument();
   });
 
-  test("sliders", () => {
-    render(<App />);
-    const sliders = screen.getAllByRole("slider");
+  test("sliders", async () => {
+    render(<Main />);
+
+    const sliders = await screen.findAllByRole("slider");
     expect(sliders.length).toBe(3);
   });
 
-  test("Time label", () => {
-    render(<App />);
-    const text = screen.getByText(/Time/i);
-    expect(text).toBeInTheDocument();
-  });
+  test("titles", async () => {
+    render(<Main />);
 
-  test("Distance label", () => {
-    render(<App />);
-    const text = screen.getByText(/Distance/i);
-    expect(text).toBeInTheDocument();
-  });
-
-  test("Pace label", () => {
-    render(<App />);
-    const text = screen.getByText(/Pace/i);
-    expect(text).toBeInTheDocument();
+    const headers = await screen.findAllByRole("heading", { level: 1 });
+    expect(headers.length).toBe(3);
   });
 });
