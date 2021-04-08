20d2498afef5822c4769b1a4b066a34001a87ce2
Bruno Henriques <bruno.henriques@hitachivantara.com>
2021-04-08 18:53:09 +0100

    test: reenable cypress tests 🕵️

--- cypress/integration/behaviour.spec.js
+++ cypress/integration/behaviour.spec.js
@@ -1,59 +1,151 @@
-describe("Clicking Distance Buttons", () => {
-  beforeEach(() => {
-    cy.visit("/");
+describe("Slider mark clicking", () => {
+  describe("Time", () => {
+    beforeEach(() => {
+      cy.visit("/");
+    });
+
+    it("12min", () => {
+      cy.get("section:nth-child(1) .MuiSlider-markLabel").contains("12'").click();
+      cy.get("section:nth-child(1) h1").contains("Time 00:12:00");
+    });
+
+    it("30min", () => {
+      cy.get("section:nth-child(1) .MuiSlider-markLabel").contains("30'").click();
+      cy.get("section:nth-child(1) h1").contains("Time 00:30:00");
+    });
+
+    it("Max (+)", () => {
+      cy.get("section:nth-child(1) .MuiSlider-markLabel").contains("+").click();
+      cy.get("section:nth-child(1) h1").contains("Time 01:00:00");
+    });
   });
 
-  it("Mile should set 1.6km", () => {
-    cy.get("button")
-      .contains("Mile")
-      .click();
+  describe("Distance", () => {
+    beforeEach(() => {
+      cy.visit("/");
+    });
 
-    cy.get("section > h1").contains("Distance 1.6km");
+    it("Mile", () => {
+      cy.get("section:nth-child(2) .MuiSlider-markLabel").contains("1 mi").click();
+      cy.get("section:nth-child(2) h1").contains("Distance 1.6km");
+    });
+
+    it("5K", () => {
+      cy.get("section:nth-child(2) .MuiSlider-markLabel").contains("5K").click();
+      cy.get("section:nth-child(2) h1").contains("Distance 5.0km");
+    });
+
+    it("Max (+)", () => {
+      cy.get("section:nth-child(2) .MuiSlider-markLabel").contains("+").click();
+      cy.get("section:nth-child(2) h1").contains("Distance 10.0km");
+    });
   });
 
-  it("50K should set 50.0km", () => {
-    cy.get("button")
-      .contains("50K")
-      .click();
+  describe("Pace", () => {
+    beforeEach(() => {
+      cy.visit("/");
+      cy.get("[aria-label=Lock]").first().click();
+    });
 
-    cy.get("section > h1").contains("Distance 50.0km");
-  });
+    it("Walk 10:00/km", () => {
+      cy.get("section:nth-child(3) .MuiSlider-markLabel").contains("🚶").click();
+      cy.get("section:nth-child(3) h1").contains("Pace 10:00/km");
+    });
 
-  it("100K should set 100.0km", () => {
-    cy.get("button")
-      .contains("100K")
-      .click();
+    it("Run 05:00/km", () => {
+      cy.get("section:nth-child(3) .MuiSlider-markLabel").contains("🏃").click();
+      cy.get("section:nth-child(3) h1").contains("Pace 05:00/km");
+    });
 
-    cy.get("section > h1").contains("Distance 100.0km");
+    it("Run fast 04:00/km", () => {
+      cy.get("section:nth-child(3) .MuiSlider-markLabel").contains("🏃💨").click();
+      cy.get("section:nth-child(3) h1").contains("Pace 04:00/km");
+    });
+
+    it("Max (+) Pace 03:45/km", () => {
+      cy.get("section:nth-child(3) .MuiSlider-markLabel").contains("+").click();
+      cy.get("section:nth-child(3) h1").contains("Pace 03:45/km");
+    });
   });
 });
 
-describe("Clicking Pace Buttons", () => {
-  beforeEach(() => {
-    cy.visit("/");
+describe("Slider growing", () => {
+  describe("Time", () => {
+    beforeEach(() => {
+      cy.visit("/");
+    });
+
+    it("growing", () => {
+      const slider = "section:nth-child(1) span[role=slider]";
+      const marks = "section:nth-child(1) .MuiSlider-markLabel";
+
+      cy.get(slider).should("have.attr", "aria-valuemax").and("eq", "3600");
+      cy.get(marks).contains("+").click();
+      cy.get(slider).should("have.attr", "aria-valuemax").and("eq", "4320");
+    });
+
+    it("growing & shrinking", () => {
+      const slider = "section:nth-child(1) span[role=slider]";
+      const marks = "section:nth-child(1) .MuiSlider-markLabel";
+
+      cy.get(slider).should("have.attr", "aria-valuemax").and("eq", "3600");
+      cy.get(marks).contains("+").click();
+      cy.get(slider).should("have.attr", "aria-valuemax").and("eq", "4320");
+      cy.get(marks).contains("12'").click();
+      cy.get(slider).should("have.attr", "aria-valuemax").and("eq", "3600");
+    });
   });
 
-  it("Walk should set 10:00/km", () => {
-    cy.get("button")
-      .contains("Walk")
-      .click();
+  describe("Distance", () => {
+    beforeEach(() => {
+      cy.visit("/");
+    });
 
-    cy.get("section > h1").contains("Pace 10:00/km");
+    it("growing", () => {
+      const slider = "section:nth-child(2) span[role=slider]";
+      const marks = "section:nth-child(2) .MuiSlider-markLabel";
+
+      cy.get(slider).should("have.attr", "aria-valuemax").and("eq", "10000");
+      cy.get(marks).contains("+").click();
+      cy.get(slider).should("have.attr", "aria-valuemax").and("eq", "12000");
+    });
+
+    it("growing & shrinking", () => {
+      const slider = "section:nth-child(2) span[role=slider]";
+      const marks = "section:nth-child(2) .MuiSlider-markLabel";
+
+      cy.get(slider).should("have.attr", "aria-valuemax").and("eq", "10000");
+      cy.get(marks).contains("+").click();
+      cy.get(slider).should("have.attr", "aria-valuemax").and("eq", "12000");
+      cy.get(marks).contains("1 mi").click();
+      cy.get(slider).should("have.attr", "aria-valuemax").and("eq", "10000");
+    });
   });
 
-  it("Run should set 05:00/km", () => {
-    cy.get("button")
-      .contains("Run")
-      .click();
+  describe("Pace", () => {
+    beforeEach(() => {
+      cy.visit("/");
+      cy.get("[aria-label=Lock]").first().click();
+    });
 
-    cy.get("section > h1").contains("Pace 05:00/km");
-  });
+    it("growing", () => {
+      const slider = "section:nth-child(3) span[role=slider]";
+      const marks = "section:nth-child(3) .MuiSlider-markLabel";
 
-  it("Bekele should set 02:30/km", () => {
-    cy.get("button")
-      .contains("Bekele")
-      .click();
+      cy.get(slider).should("have.attr", "aria-valuemax").and("eq", "16000");
+      cy.get(marks).contains("+").click();
+      cy.get(slider).should("have.attr", "aria-valuemax").and("eq", "19200");
+    });
 
-    cy.get("section > h1").contains("Pace 02:30/km");
+    it("growing & shrinking", () => {
+      const slider = "section:nth-child(3) span[role=slider]";
+      const marks = "section:nth-child(3) .MuiSlider-markLabel";
+
+      cy.get(slider).should("have.attr", "aria-valuemax").and("eq", "16000");
+      cy.get(marks).contains("+").click();
+      cy.get(slider).should("have.attr", "aria-valuemax").and("eq", "19200");
+      cy.get(marks).contains("🚶").click();
+      cy.get(slider).should("have.attr", "aria-valuemax").and("eq", "16000");
+    });
   });
 });
--- cypress/integration/structure.spec.js
+++ cypress/integration/structure.spec.js
@@ -7,17 +7,21 @@ describe("Primary DOM elements", () => {
     cy.title().should("eq", "PaceX Calculator");
   });
 
-  it("Contains a <main />!", () => {
+  it("Contains a <main />", () => {
     cy.get("main");
   });
 
-  it("Contains 3 <section /> titles!", () => {
-    cy.get("section > h1").contains("Time");
-    cy.get("section > h1").contains("Distance");
-    cy.get("section > h1").contains("Pace");
+  it("Contains 3 <section /> titles", () => {
+    cy.get("section h1").contains("Time");
+    cy.get("section h1").contains("Distance");
+    cy.get("section h1").contains("Pace");
   });
 
-  it("Contains time input!", () => {
-    cy.get("input[type=time]");
+  it("Contains 3 inputs", () => {
+    cy.get("input[type=hidden]");
+  });
+
+  it("Contains 3 sliders", () => {
+    cy.get("span[role=slider]");
   });
 });
