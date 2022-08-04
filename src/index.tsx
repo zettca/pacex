929fafcac375c417974e8638b3ae3c04a97904eb
Bruno <zettca@gmail.com>
2022-08-04 02:30:21 +0100

    remove serviceWorker

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import * as serviceWorker from "./serviceWorker";
 import App from "./App";
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
@@ -10,5 +9,3 @@ root.render(
     <App />
   </React.StrictMode>,
 );
-
-serviceWorker.unregister();
